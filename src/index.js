03b0525459ca17865d6cfa3ebd95e60ee30f7e80
Dimitri Glazkov <dimitri@glazkov.com>
2023-03-18 11:11:37 -0700

    Be less shouty.

--- src/index.js
+++ src/index.js
@@ -59,12 +59,12 @@ async function main() {
       console.error(error);
       if (interaction.replied || interaction.deferred) {
         await interaction.followUp({
-          content: "There was an error while executing this command!",
+          content: "There was an error while executing this command.",
           ephemeral: true,
         });
       } else {
         await interaction.reply({
-          content: "There was an error while executing this command!",
+          content: "There was an error while executing this command.",
           ephemeral: true,
         });
       }
