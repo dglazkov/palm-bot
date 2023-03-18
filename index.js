8faaeedaaf550238a2bfac354a00b918a9cbdc6d
Dimitri Glazkov <dimitri@glazkov.com>
2023-03-17 20:48:38 -0700

    Working bot.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { DiscussServiceClient } from "@google-cloud/generativelanguage";
 import { GoogleAuth } from "google-auth-library";
 import * as dotenv from "dotenv";
 
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
 const MODEL_NAME = "models/chat-bison-001";
 
 class Chat {
@@ -31,27 +33,60 @@ class Chat {
   }
 }
 
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 async function main() {
   dotenv.config();
+
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
   const chat = new Chat({
     model: MODEL_NAME,
     api_key: process.env.API_KEY,
   });
-  const result = await chat.call({
-    context: "Respond to all questions with a rhyming poem.",
-    examples: [
-      {
-        input: { content: "What is the capital of California?" },
-        output: {
-          content: `If the capital of California is what you seek,
-Sacramento is where you ought to peek.`,
-        },
-      },
-    ],
-    messages: [{ content: "How tall is the Eiffel Tower?" }],
+
+  client.once(Events.ClientReady, () => {
+    console.log("Ready!");
   });
 
-  console.log(result[0].candidates[0].content);
+  client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    const question = interaction.options.getString("question");
+    try {
+      await interaction.deferReply();
+      const result = await chat.call({
+        context: "Respond to all questions with a rhyming poem.",
+        examples: [
+          {
+            input: { content: "What is the capital of California?" },
+            output: {
+              content: `If the capital of California is what you seek,
+    Sacramento is where you ought to peek.`,
+            },
+          },
+        ],
+        messages: [{ content: question }],
+      });
+      await interaction.editReply(
+        `**${question}**\n${result[0].candidates[0].content}`
+      );
+    } catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      }
+    }
+  });
+
+  client.login(process.env.DISCORD_TOKEN);
 }
 
 main();
---
+++ register.js
@@ -0,0 +1,43 @@
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const appId = process.env.APPLICATION_ID;
+const token = process.env.DISCORD_TOKEN;
+
+const command = new SlashCommandBuilder()
+  .setName("poet")
+  .setDescription("Responds to all questions with a rhyming poem")
+  .addStringOption((option) =>
+    option
+      .setName("question")
+      .setDescription("The question to answer poetically")
+      .setRequired(true)
+  );
+
+const commands = [command.toJSON()];
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: "10" }).setToken(token);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(Routes.applicationCommands(appId), {
+      body: commands,
+    });
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
