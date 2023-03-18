bae41c552e61f44e26c4a16af20a1c6ecc5465b3
Dimitri Glazkov <dimitri@glazkov.com>
2023-03-17 19:11:10 -0700

    Create a `Chat` class.

--- index.js
+++ index.js
@@ -2,36 +2,53 @@ import { DiscussServiceClient } from "@google-cloud/generativelanguage";
 import { GoogleAuth } from "google-auth-library";
 import * as dotenv from "dotenv";
 
-dotenv.config();
-
 const MODEL_NAME = "models/chat-bison-001";
-const API_KEY = process.env.API_KEY;
 
-const client = new DiscussServiceClient({
-  authClient: new GoogleAuth().fromAPIKey(API_KEY),
-});
+class Chat {
+  model;
+  api_key;
+  temperature;
+  candidateCount;
+
+  constructor({ model, api_key, temperature = 0.5, candidateCount = 1 }) {
+    this.model = model;
+    this.api_key = api_key;
+    this.temperature = temperature;
+    this.candidateCount = candidateCount;
+  }
+
+  async call(prompt) {
+    const client = new DiscussServiceClient({
+      authClient: new GoogleAuth().fromAPIKey(this.api_key),
+    });
+
+    return client.generateMessage({
+      model: this.model,
+      temperature: this.temperature,
+      candidateCount: this.candidateCount,
+      prompt,
+    });
+  }
+}
 
 async function main() {
-  const result = await client.generateMessage({
-    model: MODEL_NAME, // required, which model to use to generate the result
-    temperature: 0.5, // optional, 0.0 always uses the highest-probability deastresult
-    candidateCount: 1, // optional, how many candidate results to generate
-    prompt: {
-      // optional, preamble context to prime responses
-      context: "Respond to all questions with a rhyming poem.",
-      // optional, examples to further finetune responses
-      examples: [
-        {
-          input: { content: "What is the capital of California?" },
-          output: {
-            content: `If the capital of California is what you seek,
+  dotenv.config();
+  const chat = new Chat({
+    model: MODEL_NAME,
+    api_key: process.env.API_KEY,
+  });
+  const result = await chat.call({
+    context: "Respond to all questions with a rhyming poem.",
+    examples: [
+      {
+        input: { content: "What is the capital of California?" },
+        output: {
+          content: `If the capital of California is what you seek,
 Sacramento is where you ought to peek.`,
-          },
         },
-      ],
-      // required, alternating prompt/response messages
-      messages: [{ content: "How tall is the Eiffel Tower?" }],
-    },
+      },
+    ],
+    messages: [{ content: "How tall is the Eiffel Tower?" }],
   });
 
   console.log(result[0].candidates[0].content);
