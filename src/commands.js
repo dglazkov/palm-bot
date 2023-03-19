f64a991f61e62afae38b9e1d2cd1ce62ca4424c5
Dimitri Glazkov <dimitri@glazkov.com>
2023-03-19 11:10:34 -0700

    Add error-handling test.

--- src/commands.js
+++ src/commands.js
@@ -23,17 +23,10 @@ export const questionAnswerCommand = (handler) => {
       const reply = await handler({ question, user });
       await interaction.followUp(trim(reply));
     } catch (error) {
-      if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({
-          content: "There was an error while executing this command.",
-          ephemeral: true,
-        });
-      } else {
-        await interaction.reply({
-          content: "There was an error while executing this command.",
-          ephemeral: true,
-        });
-      }
+      await interaction.followUp({
+        content: "There was an error while executing this command.",
+        ephemeral: true,
+      });
     }
   };
 };
--- tests/commands.js
+++ tests/commands.js
@@ -26,3 +26,24 @@ test("questionAnswerCommand returns results", async (t) => {
 
   await handler(interaction);
 });
+
+test("questionAnswerCommand handles errors", async (t) => {
+  const handler = questionAnswerCommand(async ({ question, user }) => {
+    throw new Error("error");
+  });
+
+  const interaction = {
+    isChatInputCommand: () => true,
+    options: {
+      getString: () => "question",
+    },
+    user: "user",
+    replied: true,
+    followUp: async ({ content, ephemeral }) => {
+      t.is(content, "There was an error while executing this command.");
+      t.true(ephemeral);
+    },
+  };
+
+  await handler(interaction);
+});
